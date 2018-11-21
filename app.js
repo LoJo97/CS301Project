eb37b68c34e99c0e3083798c455ae74e9b9df23d
lajordan3 <lajordan3@crimson.ua.edu>
2018-11-21 03:37:49 -0500

    Fixed app.js so it can now send queries. Fixed login.html to comply

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const mysql = require('mysql');
+const http = require('http'); //Server
+const url = require('url'); //URL functions
+const fs = require('fs'); //Page handling
+const mysql = require('mysql'); //MySQL handling
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//Start server
 const server = http.createServer((request, res) => {
+	//Display correct file on request
 	var q = url.parse(request.url, true);
 	var filename = '.' + q.pathname;
 
@@ -31,22 +33,28 @@ const server = http.createServer((request, res) => {
 	.on('end', () => {
 		body = Buffer.concat(body).toString();
 	});
-}).listen(port);
+}).listen(port); //Listen for requests
 
-const database = mysql.createConnection({
-	host: 'localhost',
-	username: 'root',
-	password: ''
-	//database: 'CS301_database'
-});
-
+//Handles communications between server and client
 const io = require('socket.io')(server);
 
-io.on('connection', socket => {
-	socket.on('query', (query, fn) => {
+io.on('connection', socket => { //Client connected
+	socket.on('query', (query, fn) => { //Wait for client
+		//Create database connection
+		const database = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'CS301_Project_Database'
+		});
 		database.connect(err => {
-			if(err) fn('Connection Failed\n' + err);
-			else fn("Connected!\n");
+			if(err) fn('Connection Failed: ' + err);
+			else{
+				database.query(query, (err, result, fields) => { //Perform query
+					if(err) fn(err);
+					else fn(result);
+				});
+			}
 		});
 	});
 });
